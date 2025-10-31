4bdfde7f89c871311dfaef979c0f9b7d6b2c9af9
lautiacvd-99 <lauti.acvd@gmail.com>
2025-10-31 14:11:08 -0300

    chaubackend

--- backend/routes/pedidoRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { addPedido, getPedidosByUser } from '../controllers/pedidoController';
-import { authMiddleware } from '../middlewares/authMiddleware';
-const router = Router();
-
-router.post('/', authMiddleware, addPedido);
-router.get('/', authMiddleware, getPedidosByUser);
-
-export default router;
